b2a26b85cb0aa1ab0c78b5ccf7ad2c6b6eddaf4d
Edward-19 <i1920632@continental.edu.pe>
2020-05-22 21:58:22 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-w2dg2f
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-w2dg2f)
---
+++ src/app/my-component/my.component.ts
@@ -0,0 +1,12 @@
+import{Component} from '@angular/core';
+
+@Component({
+   'selector' : 'my-component',
+   templateUrl: '/my.component.html'
+   })
+
+   export class MyComponent {
+     constructor(){
+
+     }
+   };
